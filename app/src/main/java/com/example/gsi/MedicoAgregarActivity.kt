abce285ca5b4320b7ea15c3dd5708547212f6596
UnfeelingRobot <alexisa_weird@hotmail.com>
2022-10-26 02:10:23 -0500

    agregarmedico

---
+++ app/src/main/java/com/example/gsi/MedicoAgregarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicoAgregarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medico_agregar2)
+    }
+}
