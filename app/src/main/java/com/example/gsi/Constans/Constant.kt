5f5a9bf192df033e349f7e42d5c7502e90426ef2
UnfeelingRobot <alexisa_weird@hotmail.com>
2022-11-22 11:26:57 -0500

    mensaje

--- app/src/main/java/com/example/gsi/Constans/Constant.kt
+++ app/src/main/java/com/example/gsi/Constans/Constant.kt
@@ -5,11 +5,9 @@ import com.example.gsi.Service.ApiService
 import com.example.gsi.Service.Services
 
 object Constant {
-<<<<<<< HEAD
+
     val BASE_URL = "http://192.168.1.7:8093/consultoriogsi/"
-=======
-    val BASE_URL = "http://192.168.1.5:8093/consultoriogsi/"
->>>>>>> e8c91a008516a93e20d0c86cdb95b45cd2e4cf63
+
     val NoInternet = "Connection Error"
     val retrofit = RetrofitHelper.getRetrofit().create(Services::class.java)
     val api = ApiService()
