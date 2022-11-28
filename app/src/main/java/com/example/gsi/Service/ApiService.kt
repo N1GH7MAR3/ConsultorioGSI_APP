1c8d9588cc82f0e902816eaba0a845086135f421
UnfeelingRobot <alexisa_weird@hotmail.com>
2022-11-27 22:41:18 -0500

    cambios

--- app/src/main/java/com/example/gsi/Service/ApiService.kt
+++ app/src/main/java/com/example/gsi/Service/ApiService.kt
@@ -675,7 +675,7 @@ open class ApiService {
                     (binding.rvPaciente.context as Activity).runOnUiThread {
                         val list: List<Paciente> = response.body()!!
                         iniRecyclerView(list)
-                        binding.txtPaciente.text = "Pacientes"
+                        binding.txtPaciente.text = "PACIENTES"
 
                     }
                 }
